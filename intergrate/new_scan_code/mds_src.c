525c6f212f79390368360ba1c055ecc8a804818f
Dong Dai <daidongly@gmail.com>
2021-06-21 15:52:25 -0400

    update from Dong on MDT

--- intergrate/new_scan_code/mds_src.c
+++ intergrate/new_scan_code/mds_src.c
@@ -1,6 +1,5 @@
 
 
-
 /* libraries for mds inode + attribute reading code*/
 #include <math.h>
 #include <inttypes.h>
@@ -19,23 +18,20 @@
 #include <errno.h>
 #include <time.h>
 
-
-#define FD_DEVICE "/home/saisha/Desktop/codes/aged/mds" /*mounted file system*/
+#define FD_DEVICE "./mds" /*mounted file system*/
 
 /*magic.h*/
-#define EXT4_SUPER_MAGIC 0xEF53	/*The magic number signifies what kind of filesystem it is.*/
+#define EXT4_SUPER_MAGIC 0xEF53 /*The magic number signifies what kind of filesystem it is.*/
 
 /*super block starts 1024 bytes from the begining of the disk*/
-#define BASE_OFFSET 1024 /* locates beginning of the super block (first group) */
+#define BASE_OFFSET 1024						 /* locates beginning of the super block (first group) */
 #define BLOCK_OFFSET(block) ((block)*block_size) /* to be used to retrieve locations*/
-
-
 
 /*xattr.c*/
 #define IHDR(inode)                                                \
 	((struct ext4_xattr_ibody_header *)((void *)inode +            \
 										EXT4_GOOD_OLD_INODE_SIZE + \
-										*inode.i_extra_isize   ))
+										*inode.i_extra_isize))
 
 #define IFIRST(hdr) ((struct ext4_xattr_entry *)((hdr) + 1))
 
@@ -57,29 +53,21 @@
 #define EXT4_XATTR_PAD (1 << EXT4_XATTR_PAD_BITS)
 #define EXT4_XATTR_ROUND (EXT4_XATTR_PAD - 1)
 
-
-
-
-
-
 int main(void)
 {
 
 	/*file to note the entries and values*/
-	FILE * fPtr;
+	FILE *fPtr;
 	fPtr = fopen("file1.txt", "w");
-	if(fPtr == NULL)
+	if (fPtr == NULL)
 	{
-        /* File not created hence exit */
-        printf("Unable to create file.\n");
-        exit(EXIT_FAILURE);
+		/* File not created hence exit */
+		printf("Unable to create file.\n");
+		exit(EXIT_FAILURE);
 	}
 
-
-
 	/*initializing neo4j-client*/
-	//neo4j_client_init(); 
-	
+	//neo4j_client_init();
 
 	/*accessing the disk*/
 	int fd;
@@ -91,9 +79,8 @@ int main(void)
 		exit(1); /* error while opening the device */
 	}
 
-
 	/* read super-block */
-	struct ext4_super_block super;/*superblock*/
+	struct ext4_super_block super; /*superblock*/
 	lseek(fd, BASE_OFFSET, SEEK_SET);
 	read(fd, &super, sizeof(super));
 
@@ -106,46 +93,86 @@ int main(void)
 
 	//block_size  = 2^(10 + super.s_log_block_size) : ext4 documentation
 	static unsigned int block_size = 0;
-	block_size = pow(2,10+super.s_log_block_size);
-	
+	block_size = pow(2, 10 + super.s_log_block_size);
 
 	/*reading group descriptor*/
-	struct ext4_group_desc group;/*group descriptor*/
+	struct ext4_group_desc group; /*group descriptor*/
 	lseek(fd, BLOCK_OFFSET(1), SEEK_SET);
 	read(fd, &group, sizeof(group));
 
+	struct ext4_inode cur_inode;				/*dong: struct inode is an in-memory data structure; I change it to the on-disk inode structure*/
 
-	struct inode test_inode; /*ext4 inode struct*/
-	int inode_table = group.bg_inode_table;/*get inode table location using group descriptor table*/
-	
+	int inode_table = group.bg_inode_table; /*get inode table location using group descriptor table*/
+
 	/*get total number if inodes for this filesystem using info from super block*/
 	int total_inodes = 0;
-	total_inodes= super.s_inodes_count;
-	
+	total_inodes = super.s_inodes_count;
 
 	/*get size of inode in bytes using info from super block*/
 	int inode_size = 0;
 	inode_size = super.s_inode_size;
 
-
-	int entry_count=0; /*used to check total number of entries*/
-	for (int i = 0; i< total_inodes; i++)
+	int entry_count = 0; /*used to check total number of entries*/
+	for (int i = 0; i < total_inodes; i++)
 	{
-		uint64_t inode_location = BLOCK_OFFSET(inode_table) + i*inode_size;
-				
-		lseek(fd, inode_location, SEEK_SET);
-		read(fd, &test_inode, inode_size);
+		uint64_t inode_location = BLOCK_OFFSET(inode_table) + i * inode_size;
 
-								
+		lseek(fd, inode_location, SEEK_SET);
+		read(fd, &cur_inode, inode_size);
+
+		//@todo: add code to check whether this inode is valid or not. 
+
 		/*read xattr header*/
 		struct ext4_xattr_ibody_header *header;
-		header = IHDR(&test_inode);
+		header = IHDR(&cur_inode);
 
 		/*check if the header has right magic number*/
 		if (header->h_magic == ATTR_MAGIC)
 		{
-			/*used to keep check on number if total inodes having xattr header magic number match*/			
-			entry_count ++;
+			printf("\n\n<-----------------------inode: %d------------------------------>\n", (i+1));
+			//@dong: something needed to make sure this is a Lustre Inode
+
+			// determine whether this is a directory of normal file.
+			if (S_ISDIR(cur_inode.i_mode) != 0) //S_IFDIR
+			{
+				printf("This is a directory: block_size: %u\n", block_size);
+				
+				void *block_buf = (void *) malloc (block_size);
+				int block_id = 0;
+				int reach_the_end = 0;
+
+				if (!reach_the_end && block_id < 12)
+				{
+					lseek(fd, cur_inode.i_block[block_id] * block_size, SEEK_SET);
+					read(fd, block_buf, block_size);
+					struct ext4_dir_entry_2 *ed;
+					void *p = block_buf;
+
+					while (p < block_buf + block_size)
+					{
+						ed = (struct ext4_dir_entry_2 *) p;
+						if (ed->inode != 0)
+							printf("inode: %d, rec_len: %hu, name: %s\n", ed->inode, ed->rec_len, ed->name);
+						else {
+							reach_the_end = 1;
+							break;
+						}
+						p = p + ed->rec_len;
+					}
+					block_id += 1;
+				}
+				if (block_id >= 12){
+					printf("ERROR, we encounter a huge directory and have not implemented the indirect block yet\n");
+					exit(0);
+				}
+			}
+			else if (S_ISREG(cur_inode.i_mode) != 0) //S_IFREG
+			{
+				printf("This is a file:");
+			}
+
+			/*used to keep check on number if total inodes having xattr header magic number match*/
+			entry_count++;
 
 			/*pointer to the first xattr entry*/
 			struct ext4_xattr_entry *first_entry;
@@ -153,130 +180,96 @@ int main(void)
 
 			/*end of the inode*/
 			void *end;
-			end = &test_inode + inode_size;
-
+			end = &cur_inode + inode_size;
 
 			/*"entry" is used to iterate through all the xattr entries*/
 			struct ext4_xattr_entry *entry = first_entry;
 
-
 			while (!IS_LAST_ENTRY(entry))
 			{
-		
+
 				/*printing the entry name*/
+				printf("Entry Name:");
 				for (int name_itr = 0; name_itr < entry->e_name_len; name_itr++)
 				{
-					
 					printf("%c", entry->e_name[name_itr]);
-					fprintf(fPtr,"%c",entry->e_name[name_itr]);
 				}
-				printf("\n");
-				fprintf(fPtr,"\n");
-				//fprintf(fPtr,"The value size: %d\n", entry->e_value_size);	/*printing size of value in bytes*/
-
+				printf("\t");
 
 				/*obtain and print the lov xattr value*/
-				uint64_t value_location = inode_location +EXT4_GOOD_OLD_INODE_SIZE + test_inode.i_extra_isize+ sizeof(struct ext4_xattr_ibody_header) +entry->e_value_offs;
-				
-
+				uint64_t value_location = inode_location + EXT4_GOOD_OLD_INODE_SIZE + cur_inode.i_extra_isize + sizeof(struct ext4_xattr_ibody_header) + entry->e_value_offs;
 
 				/*print the xattr value in terminal and file1.txt*/
 				unsigned char value[entry->e_value_size + 1];
 				memset(value, '\0', sizeof(char) * (entry->e_value_size + 1));
 				lseek(fd, value_location, SEEK_SET);
 				read(fd, value, entry->e_value_size);
-				printf("value: ");
-				int z = 0;
-				for (z = 0; z < entry->e_value_size; z++)
+
+				if ((char)entry->e_name[0] == 'l' && (char)entry->e_name[1] == 'm' && (char)entry->e_name[2] == 'a')
 				{
-				printf("%0.2x ", value[z]);
-				fprintf(fPtr, "%0.2x",value[z]);
+					struct lustre_mdt_attrs * lma = (struct lustre_mdt_attrs *) value;
+					printf("LMA FID: [0x%llx:0x%x:0x%x]\n", lma->lma_self_fid.f_seq, lma->lma_self_fid.f_oid, lma->lma_self_fid.f_ver);
 				}
-				printf("\n\n");
-				fprintf(fPtr, "\n\n");
-				
 
-
+				if ((char)entry->e_name[0] == 'l' && (char)entry->e_name[1] == 'i' && (char)entry->e_name[2] == 'n' && (char)entry->e_name[3] == 'k')
+				{
+					//LINK_EA_MAGIC
+					struct link_ea_header	*leh;
+					struct link_ea_entry	*lee;
+					leh = (struct link_ea_header *) value;
+					lee = (struct link_ea_entry *)(leh + 1);
+					if (leh->leh_magic == LINK_EA_MAGIC){
+						printf("Matched LinkEA\n");
+						struct lu_fid *f = (struct lu_fid *) lee->lee_parent_fid;
+						printf("name of the file: %s\t", lee->lee_name);
+						printf("parent FID: [0x%llx:0x%x:0x%x]\n", f->f_seq, f->f_oid, f->f_ver);
+					}
+					else{
+						printf("no match on LINKEA magic, error\n");
+						exit(0);
+					}
+				}
 
 				/*identify if it is lov xattr: check if the entry name is "lov"*/
-				if((char)entry->e_name[0] == 'l' && (char)entry->e_name[1] =='o' && (char)entry->e_name[2] == 'v')
+				if ((char)entry->e_name[0] == 'l' && (char)entry->e_name[1] == 'o' && (char)entry->e_name[2] == 'v')
 				{
-					struct lov_user_md lum;
-					lseek(fd, value_location, SEEK_SET);
-					read(fd, &lum, sizeof(struct lov_user_md));
-					
-					
-					
-					
-					 
-					 
-					 
-					 int ent_count =0;
-					if (lum.lmm_magic == LOV_MAGIC_COMP_V1) /*multple entries*/
+					struct lov_user_md *lum = (struct lov_user_md *) value;
+
+					int ent_count = 0;
+					if (lum->lmm_magic == LOV_MAGIC_COMP_V1) /*multple entries*/
 					{
 						//printf("%x",lum.lmm_magic);
-						 printf("This lov has multiple compenent entries");
-						 fprintf(fPtr,"It has multiple compenent entries");
+						fprintf(fPtr, "It has multiple compenent entries");
 						//comp_v1 = (struct lov_comp_md_v1 *)lum;
 						//ent_count = comp_v1->lcm_entry_count;
-						
 					}
-					else if (lum.lmm_magic == LOV_MAGIC_V1 || lum.lmm_magic == LOV_MAGIC_V3) /*single entry*/
+					else if (lum->lmm_magic == LOV_MAGIC_V1 || lum->lmm_magic == LOV_MAGIC_V3) /*single entry*/
 					{
-						// printf("%x\n",lum.lmm_magic);
-						// printf("This lov have a single compenent entry\n");
-						fprintf(fPtr,"It have a single compenent entry\n");
-						ent_count = 1;	
-					
+						fprintf(fPtr, "It have a single compenent entry\n");
+						ent_count = 1;
 					}
 
-					
-					
-						/*printing wrt lum: user_md*/
-						fprintf(fPtr,"stripe size : %d bytes\n",lum.lmm_stripe_size);
-						fprintf(fPtr,"stripe count: %d number of stripes\n\n", lum.lmm_stripe_count);
-						
-						/*information about the MDT*/
-						fprintf(fPtr,"Information about MDT\n");
-						fprintf(fPtr,"oid:%0.2x\n", lum.lmm_oi.oi.oi_id);
-						fprintf(fPtr,"o_seq:%0.2x\n", lum.lmm_oi.oi.oi_seq);
-						fprintf(fPtr,"fid_seq : %0.2x\n", lum.lmm_oi.oi_fid.f_seq);
-						fprintf(fPtr,"fid_id : %0.2x\n", lum.lmm_oi.oi_fid.f_oid);
-						fprintf(fPtr,"fid_ver : %0.2x\n", lum.lmm_oi.oi_fid.f_ver);
+					printf("LOV metadata: ");
+					printf("stripe size: %u bytes, stripe count: %hu, file layout generation: %hu\n", lum->lmm_stripe_size, lum->lmm_stripe_count, lum->lmm_layout_gen);
 
-						// /*information about the OST: per-stripe data*/
-						fprintf(fPtr,"\n\nInformation about OST\n");
-						int l =0;
-						fprintf(fPtr,"l_ost_idx %x\n", lum.lmm_objects[l].l_ost_idx); /* OST index in LOV */
-						fprintf(fPtr,"idx_gen: %x\n", lum.lmm_objects[l].l_ost_gen); /*Generation of this OST index */
+					// the comment in the data structure seems not correct
+					printf("inode id/seq: [0x%llx/0x%llx]\n", lum->lmm_oi.oi.oi_id, lum->lmm_oi.oi.oi_seq);
 
-						
-						fprintf(fPtr,"oi.oi_id: %x\n", lum.lmm_objects[l].l_ost_oi.oi.oi_id); 
-						fprintf(fPtr,"oi.oi_seq: %x\n", lum.lmm_objects[l].l_ost_oi.oi.oi_seq);
+					// /*information about the OST: per-stripe data*/
+					printf("OST objects:\n");
 
-
-
-						fprintf(fPtr,"oi_fid.f_oid: %x\n", lum.lmm_objects[l].l_ost_oi.oi_fid.f_oid);
-						fprintf(fPtr,"oi_fid.f_seq: %x\n", lum.lmm_objects[l].l_ost_oi.oi_fid.f_seq);
-						fprintf(fPtr,"oi_fid.f_ver: %x\n", lum.lmm_objects[l].l_ost_oi.oi_fid.f_ver);
-						/*---------------------------------*/
-
-						
-	
-
-
-
+					int l = 0; // Dong: there may be multiple osts, why just print the first one?; -1
+					int ost_numbers = (lum->lmm_stripe_count == 65535) ? 3 : lum->lmm_stripe_count;
+					for (l = 0; l < ost_numbers; l++)
+					{
+						printf("ost object: %d, ", l);
+						printf("ost_index: %u, ost_generation: %u, ost_object_id: %llu, ost_object_sequence: %llu\n", lum->lmm_objects[l].l_ost_idx, lum->lmm_objects[l].l_ost_gen, lum->lmm_objects[l].l_ost_oi.oi.oi_id, lum->lmm_objects[l].l_ost_oi.oi.oi_seq);
+					}
 				}
-
-				
 
 				/*iterate to the next entry*/
 				entry = EXT4_XATTR_NEXT(entry);
-
-
-
 			}
-
 		}
 	}
 
@@ -290,8 +283,3 @@ int main(void)
 
 	exit(0);
 }
-
-
-
-
-
--- intergrate/new_scan_code/xattr_sk.h
+++ intergrate/new_scan_code/xattr_sk.h
@@ -1,8 +1,5 @@
 #include <stdbool.h>
 
-
-
-
 //contains struct for internal and external attributes
 
 #define ATTR_MAGIC 0xEA020000 /*magic number to verify extended attributes*/
@@ -19,14 +16,10 @@ struct ext4_xattr_header
 	__u32 h_reserved[3]; /* zero right now */
 };
 
-
-
-
 //internal + external
 struct ext4_xattr_ibody_header
 {
 	__le32 h_magic; /* magic number for identification */
-	
 };
 
 //internal + external
@@ -41,171 +34,160 @@ struct ext4_xattr_entry
 	char e_name[0];		  /* attribute name */
 };
 
-
-
-
-
-
-
-
 // SK: April 2021
-//Lov-EA 
+//Lov-EA
 
 /*all the magic numbers related to lov xattr*/
 #define LOV_MAGIC_MAGIC 0x0BD0
-#define LOV_MAGIC_V1		(0x0BD10000 | LOV_MAGIC_MAGIC)
-#define LOV_MAGIC_V3		(0x0BD30000 | LOV_MAGIC_MAGIC)
-#define LOV_USER_MAGIC_V1	0x0BD10BD0
-#define LOV_USER_MAGIC		LOV_USER_MAGIC_V1
-#define LOV_MAGIC_COMP_V1	(0x0BD60000 | LOV_MAGIC_MAGIC)
-
-
+#define LOV_MAGIC_V1 (0x0BD10000 | LOV_MAGIC_MAGIC)
+#define LOV_MAGIC_V3 (0x0BD30000 | LOV_MAGIC_MAGIC)
+#define LOV_USER_MAGIC_V1 0x0BD10BD0
+#define LOV_USER_MAGIC LOV_USER_MAGIC_V1
+#define LOV_MAGIC_COMP_V1 (0x0BD60000 | LOV_MAGIC_MAGIC)
 
 /*--------------------------*/
-#define LOV_PATTERN_NONE	0x000
-#define LOV_PATTERN_RAID0	0x001
-#define LOV_PATTERN_RAID1	0x002
-#define LOV_PATTERN_MDT		0x100
-#define LOV_PATTERN_CMOBD	0x200
-
-
+#define LOV_PATTERN_NONE 0x000
+#define LOV_PATTERN_RAID0 0x001
+#define LOV_PATTERN_RAID1 0x002
+#define LOV_PATTERN_MDT 0x100
+#define LOV_PATTERN_CMOBD 0x200
 
 // other interdependent required structs
 
-struct lu_extent {
-       __u64 e_start;
-       __u64 e_end;
+struct lu_extent
+{
+	__u64 e_start;
+	__u64 e_end;
 };
 
-
-
-
-
-
-struct lu_fid {
-       /**
+struct lu_fid
+{
+	/**
        * FID sequence. Sequence is a unit of migration: all files (objects)
         * with FIDs from a given sequence are stored on the same server.
         * Lustre should support 2^64 objects, so even if each sequence
         * has only a single object we can still enumerate 2^64 objects.
         **/
-         __u64 f_seq;
-         /* FID number within sequence. */
-        __u32 f_oid;
-         /**
+	__u64 f_seq;
+	/* FID number within sequence. */
+	__u32 f_oid;
+	/**
          * FID version, used to distinguish different versions (in the sense
          * of snapshots, etc.) of the same file system object. Not currently
          * used.
          **/
-        __u32 f_ver;
- };
-
-
-
-struct ost_id {
-        union {
-                struct {
-                        __u64   oi_id;
-                        __u64   oi_seq;
-                } oi;
-                struct lu_fid oi_fid;
-         };
- };
-
-
-
-struct lov_ost_data_v1 {          /* per-stripe data structure (little-endian)*/
-        struct ost_id l_ost_oi;   /* OST object ID */
-        __u32 l_ost_gen;          /* generation of this l_ost_idx */
-        __u32 l_ost_idx;          /* OST index in LOV (lov_tgt_desc->tgts) */
+	__u32 f_ver;
 };
 
+struct ost_id
+{
+	union
+	{
+		struct
+		{
+			__u64 oi_id;
+			__u64 oi_seq;
+		} oi;
+		struct lu_fid oi_fid;
+	};
+};
 
+struct lustre_mdt_attrs {
+	/**
+	 * Bitfield for supported data in this structure. From enum lma_compat.
+	 * lma_self_fid and lma_flags are always available.
+	 */
+	__u32   lma_compat;
+	/**
+	 * Per-file incompat feature list. Lustre version should support all
+	 * flags set in this field. The supported feature mask is available in
+	 * LMA_INCOMPAT_SUPP.
+	 */
+	__u32   lma_incompat;
+	/** FID of this inode */
+	struct lu_fid  lma_self_fid;
+};
 
+struct lov_ost_data_v1
+{							/* per-stripe data structure (little-endian)*/
+	struct ost_id l_ost_oi; /* OST object ID */
+	__u32 l_ost_gen;		/* generation of this l_ost_idx */
+	__u32 l_ost_idx;		/* OST index in LOV (lov_tgt_desc->tgts) */
+};
 
-
-
-
-// #define LOV_MAGIC_V3	0x0BD30000 
+// #define LOV_MAGIC_V3	0x0BD30000
 // #define 	LOV_MAGIC_V1   0x0BD10BD0
-//This is struct for layout header 
-struct lov_mds_md_v1 {            /* LOV EA mds/wire data (little-endian) */
-        __u32 lmm_magic;          /* magic number = LOV_MAGIC_V1 */
-        __u32 lmm_pattern;        /* LOV_PATTERN_RAID0, LOV_PATTERN_RAID1 */
-        struct ost_id lmm_oi;   /* LOV object ID */
-	//struct ost_id;   
-     __u32 lmm_stripe_size;    /* size of stripe in bytes */
-        /* lmm_stripe_count used to be __u32 */
-        __u16 lmm_stripe_count;   /* num stripes in use for this object */
-        __u16 lmm_layout_gen;     /* layout generation number */
-        struct lov_ost_data_v1 lmm_objects[0]; /* per-stripe data */
+//This is struct for layout header
+struct lov_mds_md_v1
+{						   /* LOV EA mds/wire data (little-endian) */
+	__u32 lmm_magic;	   /* magic number = LOV_MAGIC_V1 */
+	__u32 lmm_pattern;	   /* LOV_PATTERN_RAID0, LOV_PATTERN_RAID1 */
+	struct ost_id lmm_oi;  /* LOV object ID */
+						   //struct ost_id;
+	__u32 lmm_stripe_size; /* size of stripe in bytes */
+	/* lmm_stripe_count used to be __u32 */
+	__u16 lmm_stripe_count;				   /* num stripes in use for this object */
+	__u16 lmm_layout_gen;				   /* layout generation number */
+	struct lov_ost_data_v1 lmm_objects[0]; /* per-stripe data */
 };
-
-
-
 
 //This struct is for composite entry
-struct lov_comp_md_entry_v1 {
-        __u32 lcme_id;                  /* unique identifier of component */
-        __u32 lcme_flags;               /* LCME_FL_XXX */
-        struct lu_extent lcme_extent;   /* file extent for component */
-        __u32 lcme_offset;              /* offset of component blob in layout */
-        __u32 lcme_size;                /* size of component blob data */
-        __u64 lcme_padding[2];
+struct lov_comp_md_entry_v1
+{
+	__u32 lcme_id;				  /* unique identifier of component */
+	__u32 lcme_flags;			  /* LCME_FL_XXX */
+	struct lu_extent lcme_extent; /* file extent for component */
+	__u32 lcme_offset;			  /* offset of component blob in layout */
+	__u32 lcme_size;			  /* size of component blob data */
+	__u64 lcme_padding[2];
 };
-
-
 
 // #define LOV_MAGIC_COMP_V1 0x0BD40BD0
 //This struct is for composite header
-struct lov_comp_md_v1 {
-	__u32	lcm_magic;      /* LOV_USER_MAGIC_COMP_V1 */
-	__u32	lcm_size;       /* overall size including this struct */
-	__u32	lcm_layout_gen;
-	__u16	lcm_flags;
-	__u16	lcm_entry_count;
+struct lov_comp_md_v1
+{
+	__u32 lcm_magic; /* LOV_USER_MAGIC_COMP_V1 */
+	__u32 lcm_size;	 /* overall size including this struct */
+	__u32 lcm_layout_gen;
+	__u16 lcm_flags;
+	__u16 lcm_entry_count;
 	/* lcm_mirror_count stores the number of actual mirrors minus 1,
 	 * so that non-flr files will have value 0 meaning 1 mirror. */
-	__u16	lcm_mirror_count;
-	__u16	lcm_padding1[3];
-	__u64	lcm_padding2;
+	__u16 lcm_mirror_count;
+	__u16 lcm_padding1[3];
+	__u64 lcm_padding2;
 	struct lov_comp_md_entry_v1 lcm_entries[0];
 } __attribute__((packed));
 
-
-
-
 /*from inlcude/uapi/linux/lustre/lustre_user.h*/
 
-
 #define lov_user_ost_data lov_user_ost_data_v1
-struct lov_user_ost_data_v1 {     /* per-stripe data structure */
-	struct ost_id l_ost_oi;	  /* OST object ID */
-	__u32 l_ost_gen;          /* generation of this OST index */
-	__u32 l_ost_idx;          /* OST index in LOV */
+struct lov_user_ost_data_v1
+{							/* per-stripe data structure */
+	struct ost_id l_ost_oi; /* OST object ID */
+	__u32 l_ost_gen;		/* generation of this OST index */
+	__u32 l_ost_idx;		/* OST index in LOV */
 } __attribute__((packed));
 
 #define lov_user_md lov_user_md_v1
-struct lov_user_md_v1 {           /* LOV EA user data (host-endian) */
-	__u32 lmm_magic;          /* magic number = LOV_USER_MAGIC_V1 */
-	__u32 lmm_pattern;        /* LOV_PATTERN_RAID0, LOV_PATTERN_RAID1 */
-	struct ost_id lmm_oi;	  /* MDT parent inode id/seq (id/0 for 1.x) */
-	__u32 lmm_stripe_size;    /* size of stripe in bytes */
-	__u16 lmm_stripe_count;   /* num stripes in use for this object */
-	union {
-		__u16 lmm_stripe_offset;  /* starting stripe offset in
+struct lov_user_md_v1
+{							/* LOV EA user data (host-endian) */
+	__u32 lmm_magic;		/* magic number = LOV_USER_MAGIC_V1 */
+	__u32 lmm_pattern;		/* LOV_PATTERN_RAID0, LOV_PATTERN_RAID1 */
+	struct ost_id lmm_oi;	/* MDT parent inode id/seq (id/0 for 1.x) */
+	__u32 lmm_stripe_size;	/* size of stripe in bytes */
+	__u16 lmm_stripe_count; /* num stripes in use for this object */
+	union
+	{
+		__u16 lmm_stripe_offset; /* starting stripe offset in
 					   * lmm_objects, use when writing */
-		__u16 lmm_layout_gen;     /* layout generation number
+		__u16 lmm_layout_gen;	 /* layout generation number
 					   * used when reading */
 	};
 	struct lov_user_ost_data_v1 lmm_objects[0]; /* per-stripe data */
-} __attribute__((packed,  __may_alias__));
-
-
+} __attribute__((packed, __may_alias__));
 
 /*from liblustre_layoutapi.c*/
-
-
 
 /**
  * An Opaque data type abstracting the layout of a Lustre file.
@@ -221,8 +203,111 @@ struct lov_user_md_v1 {           /* LOV EA user data (host-endian) */
 // 	struct list_head	  llot_comp_list;
 // };
 
+struct ext4_inode {
+	__le16	i_mode;		/* File mode */
+	__le16	i_uid;		/* Low 16 bits of Owner Uid */
+	__le32	i_size_lo;	/* Size in bytes */
+	__le32	i_atime;	/* Access time */
+	__le32	i_ctime;	/* Inode Change time */
+	__le32	i_mtime;	/* Modification time */
+	__le32	i_dtime;	/* Deletion Time */
+	__le16	i_gid;		/* Low 16 bits of Group Id */
+	__le16	i_links_count;	/* Links count */
+	__le32	i_blocks_lo;	/* Blocks count */
+	__le32	i_flags;	/* File flags */
+	union {
+		struct {
+			__le32  l_i_version;
+		} linux1;
+		struct {
+			__u32  h_i_translator;
+		} hurd1;
+		struct {
+			__u32  m_i_reserved1;
+		} masix1;
+	} osd1;				/* OS dependent 1 */
+	__le32	i_block[EXT4_N_BLOCKS];/* Pointers to blocks */
+	__le32	i_generation;	/* File version (for NFS) */
+	__le32	i_file_acl_lo;	/* File ACL */
+	__le32	i_size_high;
+	__le32	i_obso_faddr;	/* Obsoleted fragment address */
+	union {
+		struct {
+			__le16	l_i_blocks_high; /* were l_i_reserved1 */
+			__le16	l_i_file_acl_high;
+			__le16	l_i_uid_high;	/* these 2 fields */
+			__le16	l_i_gid_high;	/* were reserved2[0] */
+			__le16	l_i_checksum_lo;/* crc32c(uuid+inum+inode) LE */
+			__le16	l_i_reserved;
+		} linux2;
+		struct {
+			__le16	h_i_reserved1;	/* Obsoleted fragment number/size which are removed in ext4 */
+			__u16	h_i_mode_high;
+			__u16	h_i_uid_high;
+			__u16	h_i_gid_high;
+			__u32	h_i_author;
+		} hurd2;
+		struct {
+			__le16	h_i_reserved1;	/* Obsoleted fragment number/size which are removed in ext4 */
+			__le16	m_i_file_acl_high;
+			__u32	m_i_reserved2[2];
+		} masix2;
+	} osd2;				/* OS dependent 2 */
+	__le16	i_extra_isize;
+	__le16	i_checksum_hi;	/* crc32c(uuid+inum+inode) BE */
+	__le32  i_ctime_extra;  /* extra Change time      (nsec << 2 | epoch) */
+	__le32  i_mtime_extra;  /* extra Modification time(nsec << 2 | epoch) */
+	__le32  i_atime_extra;  /* extra Access time      (nsec << 2 | epoch) */
+	__le32  i_crtime;       /* File Creation time */
+	__le32  i_crtime_extra; /* extra FileCreationtime (nsec << 2 | epoch) */
+	__le32  i_version_hi;	/* high 32 bits for 64-bit version */
+};
+
+
+/*
+ * Structure of a directory entry
+ */
+#define EXT4_NAME_LEN 255
+
+struct ext4_dir_entry {
+	__le32	inode;			/* Inode number */
+	__le16	rec_len;		/* Directory entry length */
+	__le16	name_len;		/* Name length */
+	char	name[EXT4_NAME_LEN];	/* File name */
+};
+
+/*
+ * The new version of the directory entry.  Since EXT4 structures are
+ * stored in intel byte order, and the name_len field could never be
+ * bigger than 255 chars, it's safe to reclaim the extra byte for the
+ * file_type field.
+ */
+struct ext4_dir_entry_2 {
+	__le32	inode;			/* Inode number */
+	__le16	rec_len;		/* Directory entry length */
+	__u8	name_len;		/* Name length */
+	__u8	file_type;
+	char	name[EXT4_NAME_LEN];	/* File name */
+};
 
 
 
+/** The link ea holds 1 \a link_ea_entry for each hardlink */
+#define LINK_EA_MAGIC 0x11EAF1DFUL
+struct link_ea_header {
+	__u32 leh_magic;
+	__u32 leh_reccount;
+	__u64 leh_len;	/* total size */
+	__u32 leh_overflow_time;
+	__u32 leh_padding;
+};
 
-
+/** Hardlink data is name and parent fid.
+ * Stored in this crazy struct for maximum packing and endian-neutrality
+ */
+struct link_ea_entry {
+        /** __u16 stored big-endian, unaligned */
+        unsigned char      lee_reclen[2];
+        unsigned char      lee_parent_fid[sizeof(struct lu_fid)];
+        char               lee_name[0];
+} __attribute__((packed));
